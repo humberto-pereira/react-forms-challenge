2174cc83c8b74469673f08ab832c7a4b4316aa3c
Humberto Pereira <humberto_pereira@live.com>
2023-12-17 22:08:45 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm.js";
+import Content from "./components/Content";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <Sidebar />
-    </div>
-  );
+    return (
+        <div className={css.App}>
+            {/* Add your components here */}
+            <NavBarForm />
+            <Sidebar />
+            <Content />
+        </div>
+    );
 }
 
 export default App;
